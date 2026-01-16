e59a2a8a142ed1628fcb93220d3bd74ab6ec78a3
unknown <mu0826960@gmail.com>
2026-01-17 02:14:10 +0500

    first program

---
+++ hello.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+ 
+int main() {
+    // This is a comment 
+    /*
+        This is Multi Line Comment
+    */
+    std::cout << "Hello World" << std::endl;
+    std::cout << "First C++";
+    return 0;
+}
