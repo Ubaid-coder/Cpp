88997d733ebe3478bdcff1f05a8f868cfff509ec
unknown <mu0826960@gmail.com>
2026-01-17 02:14:56 +0500

    const

---
+++ const.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+
+int main() {
+
+    // double pi = 3.142;
+    // pi = 4;
+    const double pi = 3.142;
+    double radius = 10;
+    double circumference = 2 * pi * radius;
+
+    std::cout << circumference << "cm";
+
+    return 0;
+}
