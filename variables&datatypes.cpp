5cc69b197c682493d4b564cafca6b1b64ae288e6
unknown <mu0826960@gmail.com>
2026-01-17 02:14:35 +0500

    variables and datatypes

---
+++ variables&datatypes.cpp
@@ -0,0 +1,52 @@
+#include <iostream>
+
+int main(){
+    // Integer
+    int x; // Declaration
+    x = 5;
+    int y = 6;
+    int sum = x + y;
+
+    int age = 16;
+    int year = 2026;
+    int days = 7.5;
+
+    // dobule 
+    double price = 10.99;
+    double gpa = 2.5;
+    double temperature = 25.1;
+
+
+    //Single Character
+    char grade = 'A';
+    char initial = 'C';
+    char currency = '$';
+
+    // boolean (True OR False)
+    bool student = true;
+    bool lightswitch = false;
+    bool power = true;
+    bool forsale = true;
+
+    // string 
+    std::string name = "Ubaid";
+    std::string day = "Friday";
+    std::string food = "Burger";
+    std::string address = "L-562 5-C/3";
+
+    std::cout << "Hello " << name << '\n';
+    std::cout << "You are " << age << " years old";
+    
+    // std::cout << name;
+
+    // std::cout << initial;
+
+    // std::cout << price;
+    // std::cout << days;
+
+    // std::cout << x << '\n';
+    // std::cout << y << '\n';
+    // std::cout << sum;
+
+    return 0;
+}
