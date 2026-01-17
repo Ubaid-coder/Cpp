0fbdc37b2e900cc14b809357ddb493ff00922996
unknown <mu0826960@gmail.com>
2026-01-18 01:10:12 +0500

    hypotenuse calculator

---
+++ hypotenuse calculator program.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+#include <cmath>
+
+
+int main() {    
+
+    double a, b, c;
+
+    std::cout << "Enter side A: ";
+    std::cin >> a;
+
+    std::cout << "Enter side B: ";
+    std::cin >> b;
+
+    // a = pow(a,2);
+    // b = pow(b,2);
+
+    c = sqrt(pow(a,2)+ pow(b,2));
+
+    std::cout << "Side C is: " << c;
+
+    return 0;
+}
