ee3fe8c7a029857185cdf63c4d1e9b3fe87b576e
unknown <mu0826960@gmail.com>
2026-01-20 14:23:04 +0500

    logical operators

---
+++ logical operator.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+
+int main()
+{
+
+    /*
+     && = check if two conditions are true
+     || = check if at least one of two conditions is ture
+      ! = reverse the logical state of its operand
+    */
+
+    int temp;
+    bool sunny = true;
+
+    std::cout << "Enter the temperature: ";
+    std::cin >> temp;
+
+    if (temp > 0 && temp < 30)
+    {
+        std::cout << "The temperature is good! \n";
+    }
+    else
+    {
+        std::cout << "The temperature is bad \n";
+    }
+
+    if (!sunny){
+        std::cout << "It is sunny outside! \n";
+    }else{
+        std::cout << "It is cloudy outside! \n";
+    }
+        return 0;
+}
