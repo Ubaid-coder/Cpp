022b6b2ccb25cf1393d006aa172e5986c63ed166
unknown <mu0826960@gmail.com>
2026-01-22 02:42:49 +0500

    overloaded function

---
+++ overloaded function.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+
+void bakePizza();
+void bakePizza(std::string topping1);
+void bakePizza(std::string topping1, std::string topping2);
+
+int main()
+{
+
+    bakePizza("large", "mushroom");
+
+    return 0;
+}
+
+void bakePizza()
+{
+    std::cout << "here is your pizza!\n";
+}
+
+void bakePizza(std::string topping1)
+{
+    std::cout << "Here is your " << topping1 << " pizza \n";
+}
+
+void bakePizza(std::string topping1, std::string topping2)
+{
+    std::cout << "Here is your " << topping1 << " and " << topping2 << " pizza \n";
+}
