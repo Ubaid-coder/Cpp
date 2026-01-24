d18ebaf33eec8038d011ef3f01d9288b98e14cf4
unknown <mu0826960@gmail.com>
2026-01-25 04:30:14 +0500

    iteration over array

---
+++ iterate over an array.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+
+int main(){
+
+    std::string students[] = {"Ubaid", "Shiraz", "Shaheer", "Rohan"};
+
+    for(int i=0; i < sizeof(students) / sizeof(std::string); i++){
+        std::cout << students[i] << "\n";
+    }
+
+    char grades[] = {'A', 'B', 'C', 'D', 'F'};
+    for(int i=0; i < sizeof(grades) / sizeof(char); i++ ){
+        std::cout << grades[i] << "\n";
+    }
+    
+    return 0;
+}
