c3106b01e3570ebd1a56cf66b52268734e05cf6b
unknown <mu0826960@gmail.com>
2026-01-28 14:33:43 +0500

    constructor overloading

---
+++ constructor overloading.cpp
@@ -0,0 +1,35 @@
+#include <iostream>
+
+class Pizza{
+    public:
+        std::string topping1;
+        std::string topping2;
+
+    Pizza(std::string topping1){
+        this-> topping1 = topping1;
+    }
+    Pizza(std::string topping1, std::string topping2){
+        this-> topping1 = topping1;
+        this-> topping2 = topping2;
+    }
+
+    Pizza(){
+        
+    }
+};
+
+int main(){
+
+    Pizza pizza1("pepperoni");
+    Pizza pizza2("mushrooms", "peppers");
+    Pizza pizza3;
+
+    std::cout << pizza1.topping1 << '\n';
+
+    std::cout << pizza2.topping1 << '\n';
+    std::cout << pizza2.topping2 << '\n';
+
+
+
+    return 0;
+}
