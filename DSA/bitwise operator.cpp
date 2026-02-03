d63085fcb7ecb13746b292af7a01df0c75db9f87
unknown <mu0826960@gmail.com>
2026-02-04 01:12:01 +0500

    bitwise operator

---
+++ DSA/bitwise operator.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+
+int main(){
+    int a = 3, b = 7;
+    std::cout << (a == 4 )&&( b == 7) << '\n';
+    std::cout << (a == 4 )||( b == 7) << '\n';
+    std::cout << 10 << 2 << '\n';
+
+    std::cout << -2*6 << '\n';
+    std::cout << (5-2)*6 << '\n';
+    return 0;
+}
